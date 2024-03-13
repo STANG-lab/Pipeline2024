dbfc317b131e595dfacf49f5121259ba65766b8b
ryan-partlan <ryanorion27@gmail.com>
2024-03-13 15:21:16 -0400

    Reformatted files, pulled out dict_utils from 004

--- Feat_extraction/Transcript_006b1_DiscourseFeatures_strgraph.py
+++ Feat_extraction/Transcript_006b1_DiscourseFeatures_strgraph.py
@@ -9,8 +9,10 @@ import random
 import os
 import string
 from spacy.lang.en import English
+
 nlp = English()
 import spacy
+
 nlp = spacy.load("en_core_web_sm")
 import networkx as nx
 from statistics import mean
@@ -24,15 +26,17 @@ from statistics import stdev
 root_data = "C://Users//ANikzad//Desktop//Local_Pipeline//Data//"
 
 ### Specify Source Folder
-    # --- Remora
+# --- Remora
 word_level_location = "Remora-2023//Batch-1//3_word_aggregates//"
 
 ### Specify Destination folder
-    # --- Remora
+# --- Remora
 dicourse_feature_location = "Remora-2023//Batch-1//6_discourse_level_features//"
 
-drive_aggregate_in_path = root_data+word_level_location
-drive_discourselevel_graph_out_path = root_data+dicourse_feature_location+"6b_graphs//6b1_strgraphs//"
+drive_aggregate_in_path = root_data + word_level_location
+drive_discourselevel_graph_out_path = (
+    root_data + dicourse_feature_location + "6b_graphs//6b1_strgraphs//"
+)
 
 
 # In[5]:
@@ -48,78 +52,83 @@ len(all_aggregate_files)
 
 ### graph feature function ###
 def graph_features(aggregate, G_seq):
-    entry={}
-    entry['uid'] = aggregate.split('.')[0]
-    entry['g_sq_nodes'] = G_seq.number_of_nodes()
-    entry['g_sq_edges'] = G_seq.number_of_edges()
-    
+    entry = {}
+    entry["uid"] = aggregate.split(".")[0]
+    entry["g_sq_nodes"] = G_seq.number_of_nodes()
+    entry["g_sq_edges"] = G_seq.number_of_edges()
+
     try:
-        entry['g_sq_degree'] = sum([d for (n,d) in nx.degree(G_seq)]) / float(G_seq.number_of_nodes())
+        entry["g_sq_degree"] = sum([d for (n, d) in nx.degree(G_seq)]) / float(
+            G_seq.number_of_nodes()
+        )
     except:
-        entry['g_sq_degree'] = 0
+        entry["g_sq_degree"] = 0
     try:
-        entry['g_sq_density'] = nx.density(G_seq)
+        entry["g_sq_density"] = nx.density(G_seq)
     except:
-        entry['g_sq_density'] = 0
+        entry["g_sq_density"] = 0
     try:
-        entry['g_sq_diameter'] = nx.diameter(nx.to_undirected(G_seq))
+        entry["g_sq_diameter"] = nx.diameter(nx.to_undirected(G_seq))
     except:
-        entry['g_sq_diameter'] = 0
+        entry["g_sq_diameter"] = 0
     try:
-        entry['g_sq_aspl'] = nx.average_shortest_path_length(nx.to_undirected(G_seq))
-    except:
-        pass
-    try:
-        entry['g_sq_lscc'] = len(max(nx.strongly_connected_components(G_seq), key=len))
+        entry["g_sq_aspl"] = nx.average_shortest_path_length(nx.to_undirected(G_seq))
     except:
         pass
-    
+    try:
+        entry["g_sq_lscc"] = len(max(nx.strongly_connected_components(G_seq), key=len))
+    except:
+        pass
+
     parallel_edge = 0
     l2 = 0
     l3 = 0
     for node1 in nx.nodes(G_seq):
-        #print(node1)
+        # print(node1)
         for node2 in nx.neighbors(G_seq, node1):
-            #print('\t', node2)
-            parallel_edge = parallel_edge + (G_seq.number_of_edges(u=node1, v=node2) - 1)
+            # print('\t', node2)
+            parallel_edge = parallel_edge + (
+                G_seq.number_of_edges(u=node1, v=node2) - 1
+            )
             if node1 in nx.neighbors(G_seq, node2):
-                l2+=1
+                l2 += 1
             for node3 in nx.neighbors(G_seq, node2):
                 if node1 in nx.neighbors(G_seq, node3):
                     if node1 != node2 and node2 != node3:
-                        l3+=1
-    entry['g_sq_pe'] = parallel_edge
-    entry['g_sq_l1'] = nx.number_of_selfloops(G_seq)
-    entry['g_sq_l2'] = l2/2
-    entry['g_sq_l3'] = l3/3
+                        l3 += 1
+    entry["g_sq_pe"] = parallel_edge
+    entry["g_sq_l1"] = nx.number_of_selfloops(G_seq)
+    entry["g_sq_l2"] = l2 / 2
+    entry["g_sq_l3"] = l3 / 3
 
     G_seq2 = nx.Graph()
-    for u,v,data in G_seq.edges(data=True):
-        w = data['weight'] if 'weight' in data else 1.0
-        if G_seq2.has_edge(u,v):
-            G_seq2[u][v]['weight'] += w
+    for u, v, data in G_seq.edges(data=True):
+        w = data["weight"] if "weight" in data else 1.0
+        if G_seq2.has_edge(u, v):
+            G_seq2[u][v]["weight"] += w
         else:
             G_seq2.add_edge(u, v, weight=w)
     try:
-        entry['g_sq_cc'] = nx.average_clustering(G_seq2.to_directed())
+        entry["g_sq_cc"] = nx.average_clustering(G_seq2.to_directed())
     except:
         pass
     try:
-        entry['g_sq_largestclique'] = nx.approximation.large_clique_size(G_seq2)
+        entry["g_sq_largestclique"] = nx.approximation.large_clique_size(G_seq2)
     except:
         pass
-    
+
     diameter_rand = []
     aspl_rand = []
     lscc_rand = []
-    #lcc_rand = []
-    #ncc_rand = []
+    # lcc_rand = []
+    # ncc_rand = []
     cc_rand = []
 
-
-    for seedidx in range(0,200):
-        seed = random.randint(24, 5000) # Ensures random selection of seeded graphs
-        G_rand = nx.gnm_random_graph(entry['g_sq_nodes'], entry['g_sq_edges'], seed=seed, directed=True)
+    for seedidx in range(0, 200):
+        seed = random.randint(24, 5000)  # Ensures random selection of seeded graphs
+        G_rand = nx.gnm_random_graph(
+            entry["g_sq_nodes"], entry["g_sq_edges"], seed=seed, directed=True
+        )
         try:
             diameter_rand.append(nx.diameter(nx.to_undirected((G_rand))))
         except:
@@ -133,27 +142,30 @@ def graph_features(aggregate, G_seq):
         except:
             pass
         try:
-            lscc_rand.append(len(max(nx.strongly_connected_components(G_rand), key=len)))
+            lscc_rand.append(
+                len(max(nx.strongly_connected_components(G_rand), key=len))
+            )
         except:
             pass
     try:
-        entry['g_sq_zlscc'] = (entry['g_sq_lscc']-mean(lscc_rand))/stdev(lscc_rand)
+        entry["g_sq_zlscc"] = (entry["g_sq_lscc"] - mean(lscc_rand)) / stdev(lscc_rand)
     except:
         pass
     try:
-        entry['g_sq_zdiameter'] = (entry['g_sq_diameter']-mean(lscc_rand))/stdev(lscc_rand)
+        entry["g_sq_zdiameter"] = (entry["g_sq_diameter"] - mean(lscc_rand)) / stdev(
+            lscc_rand
+        )
     except:
         pass
     try:
-        entry['g_sq_zaspl'] = (entry['g_sq_aspl']-mean(lscc_rand))/stdev(lscc_rand)
+        entry["g_sq_zaspl"] = (entry["g_sq_aspl"] - mean(lscc_rand)) / stdev(lscc_rand)
     except:
         pass
     try:
-        entry['g_sq_zcc'] = (entry['g_sq_cc']-mean(lscc_rand))/stdev(lscc_rand)
+        entry["g_sq_zcc"] = (entry["g_sq_cc"] - mean(lscc_rand)) / stdev(lscc_rand)
     except:
         pass
     return entry
-    
 
 
 # In[7]:
@@ -162,63 +174,69 @@ def graph_features(aggregate, G_seq):
 ### structural graph features
 df_strgfeatures = pd.DataFrame()
 for aggregate in all_aggregate_files:
-    if aggregate.split('_')[5] in [ 'JOU', 'SOC', 'PIC']:
-        #print(aggregate.split('_')[5])
-        #print(drive_aggregate_in_path+aggregate)
-        df = pd.read_csv(drive_aggregate_in_path+aggregate)
+    if aggregate.split("_")[5] in ["JOU", "SOC", "PIC"]:
+        # print(aggregate.split('_')[5])
+        # print(drive_aggregate_in_path+aggregate)
+        df = pd.read_csv(drive_aggregate_in_path + aggregate)
         df["row_id"] = df.index
-        df = df.loc[df['is_unintelligible'] == 0].copy()
-        #df = df.loc[df['is_repetition'] == 0].copy()
-        df = df.loc[df['is_partial'] == 0].copy()
-        df = df.loc[df['is_punctuation'] == 0].copy()
-        df = df.loc[df['is_noise'] == 0].copy()
-        df = df.loc[df['is_laugh'] == 0].copy()
-        df = df.loc[df['is_filledpause'] == 0].copy()
+        df = df.loc[df["is_unintelligible"] == 0].copy()
+        # df = df.loc[df['is_repetition'] == 0].copy()
+        df = df.loc[df["is_partial"] == 0].copy()
+        df = df.loc[df["is_punctuation"] == 0].copy()
+        df = df.loc[df["is_noise"] == 0].copy()
+        df = df.loc[df["is_laugh"] == 0].copy()
+        df = df.loc[df["is_filledpause"] == 0].copy()
 
-        #df = df[['uid', 'speaker','sentence_id', 'token_id', 'content',]]
-        df = df.loc[df['speaker'] == 'Subject'].copy().reset_index()[['sentence_id', 'token_id', 'content']]
+        # df = df[['uid', 'speaker','sentence_id', 'token_id', 'content',]]
+        df = (
+            df.loc[df["speaker"] == "Subject"]
+            .copy()
+            .reset_index()[["sentence_id", "token_id", "content"]]
+        )
         node_sequence = []
         for index, row in df.iterrows():
-            content = row['content']
+            content = row["content"]
             content_nlp = nlp(content)
             for each_nlp in content_nlp:
-                #print(row['token_id'], row['sentence_id'],each_nlp.lemma_)
+                # print(row['token_id'], row['sentence_id'],each_nlp.lemma_)
                 node_sequence.append(each_nlp.lemma_)
-        node_sequence = ' '.join(node_sequence).replace(' - ', '-').split(' ')
-        with open(drive_discourselevel_graph_out_path+aggregate.split('.')[0]+'.txt', 'w') as node_list:
+        node_sequence = " ".join(node_sequence).replace(" - ", "-").split(" ")
+        with open(
+            drive_discourselevel_graph_out_path + aggregate.split(".")[0] + ".txt", "w"
+        ) as node_list:
             for each_node in node_sequence:
                 node_list.write(each_node)
-                node_list.write('\n')
+                node_list.write("\n")
         G_seq = nx.MultiDiGraph()
 
-        for i in range (0, len(node_sequence)-1):
-            G_seq.add_edge(node_sequence[i], node_sequence[i+1], weight=1)
-        
+        for i in range(0, len(node_sequence) - 1):
+            G_seq.add_edge(node_sequence[i], node_sequence[i + 1], weight=1)
+
         entry = graph_features(aggregate, G_seq)
-        
+
         dynamic_entry = []
-        m=0
+        m = 0
         if len(node_sequence) > 40:
-            #print(len(node_sequence))
+            # print(len(node_sequence))
             while m < (len(node_sequence) - 30):
-                window_nodes = node_sequence[m:m+30]
-                m=m+10
+                window_nodes = node_sequence[m : m + 30]
+                m = m + 10
                 G_seq30 = nx.MultiDiGraph()
-                for i in range (0, len(window_nodes)-1):
-                    G_seq30.add_edge(window_nodes[i], window_nodes[i+1], weight=1)
-                dynamic_entry.append(graph_features(aggregate,G_seq30))
-            if m != (len(node_sequence) -1):
+                for i in range(0, len(window_nodes) - 1):
+                    G_seq30.add_edge(window_nodes[i], window_nodes[i + 1], weight=1)
+                dynamic_entry.append(graph_features(aggregate, G_seq30))
+            if m != (len(node_sequence) - 1):
                 window_nodes = node_sequence[-30:]
                 G_seq30 = nx.MultiDiGraph()
-                for i in range (0, len(window_nodes)-1):
-                    G_seq30.add_edge(window_nodes[i], window_nodes[i+1], weight=1)
-                dynamic_entry.append(graph_features(aggregate,G_seq30))
-                
+                for i in range(0, len(window_nodes) - 1):
+                    G_seq30.add_edge(window_nodes[i], window_nodes[i + 1], weight=1)
+                dynamic_entry.append(graph_features(aggregate, G_seq30))
+
             g_sq_nodes = []
             g_sq_edges = []
             g_sq_degree = []
-            g_sq_density= []
-            g_sq_diameter= []
+            g_sq_density = []
+            g_sq_diameter = []
             g_sq_aspl = []
             g_sq_lscc = []
             g_sq_pe = []
@@ -232,66 +250,67 @@ for aggregate in all_aggregate_files:
             g_sq_zaspl = []
             g_sq_zcc = []
             for each_entry in dynamic_entry:
-                g_sq_nodes.append(each_entry['g_sq_nodes'])
-                g_sq_edges.append(each_entry['g_sq_edges'])
-                g_sq_degree.append(each_entry['g_sq_degree'])
-                g_sq_density.append(each_entry['g_sq_density'])
-                g_sq_diameter.append(each_entry['g_sq_diameter'])
-                g_sq_aspl.append(each_entry['g_sq_aspl'])
-                g_sq_lscc.append(each_entry['g_sq_lscc'])
-                g_sq_pe.append(each_entry['g_sq_pe'])
-                g_sq_l1.append(each_entry['g_sq_l1'])
-                g_sq_l2.append(each_entry['g_sq_l2'])
-                g_sq_l3.append(each_entry['g_sq_l3'])
-                g_sq_cc.append(each_entry['g_sq_cc'])
-                g_sq_largestclique.append(each_entry['g_sq_largestclique'])
-                g_sq_zlscc.append(each_entry['g_sq_zlscc'])
-                g_sq_zdiameter.append(each_entry['g_sq_zdiameter'])
-                g_sq_zaspl.append(each_entry['g_sq_zaspl'])
-                g_sq_zcc.append(each_entry['g_sq_zcc'])
+                g_sq_nodes.append(each_entry["g_sq_nodes"])
+                g_sq_edges.append(each_entry["g_sq_edges"])
+                g_sq_degree.append(each_entry["g_sq_degree"])
+                g_sq_density.append(each_entry["g_sq_density"])
+                g_sq_diameter.append(each_entry["g_sq_diameter"])
+                g_sq_aspl.append(each_entry["g_sq_aspl"])
+                g_sq_lscc.append(each_entry["g_sq_lscc"])
+                g_sq_pe.append(each_entry["g_sq_pe"])
+                g_sq_l1.append(each_entry["g_sq_l1"])
+                g_sq_l2.append(each_entry["g_sq_l2"])
+                g_sq_l3.append(each_entry["g_sq_l3"])
+                g_sq_cc.append(each_entry["g_sq_cc"])
+                g_sq_largestclique.append(each_entry["g_sq_largestclique"])
+                g_sq_zlscc.append(each_entry["g_sq_zlscc"])
+                g_sq_zdiameter.append(each_entry["g_sq_zdiameter"])
+                g_sq_zaspl.append(each_entry["g_sq_zaspl"])
+                g_sq_zcc.append(each_entry["g_sq_zcc"])
 
-            entry['g_30sq10_nodes'] = mean(g_sq_nodes)
-            entry['g_30sq10_edges'] = mean(g_sq_edges)
-            entry['g_30sq10_degree'] = mean(g_sq_degree)
-            entry['g_30sq10_density'] = mean(g_sq_density)
-            entry['g_30sq10_diameter'] = mean(g_sq_diameter)
-            entry['g_30sq10_aspl'] = mean(g_sq_aspl)
-            entry['g_30sq10_lscc'] = mean(g_sq_lscc)
-            entry['g_30sq10_pe'] = mean(g_sq_pe)
-            entry['g_30sq10_l1'] = mean(g_sq_l1)
-            entry['g_30sq10_l2'] = mean(g_sq_l2)
-            entry['g_30sq10_l3'] = mean(g_sq_l3)
-            entry['g_30sq10_cc'] = mean(g_sq_cc)
-            entry['g_30sq10_largestclique'] = mean(g_sq_largestclique)
-            entry['g_30sq10_zlscc'] = mean(g_sq_zlscc)
-            entry['g_30sq10_zdiameter'] = mean(g_sq_zdiameter)
-            entry['g_30sq10_zaspl'] = mean(g_sq_zaspl)
-            entry['g_30sq10_zcc'] = mean(g_sq_zcc)
+            entry["g_30sq10_nodes"] = mean(g_sq_nodes)
+            entry["g_30sq10_edges"] = mean(g_sq_edges)
+            entry["g_30sq10_degree"] = mean(g_sq_degree)
+            entry["g_30sq10_density"] = mean(g_sq_density)
+            entry["g_30sq10_diameter"] = mean(g_sq_diameter)
+            entry["g_30sq10_aspl"] = mean(g_sq_aspl)
+            entry["g_30sq10_lscc"] = mean(g_sq_lscc)
+            entry["g_30sq10_pe"] = mean(g_sq_pe)
+            entry["g_30sq10_l1"] = mean(g_sq_l1)
+            entry["g_30sq10_l2"] = mean(g_sq_l2)
+            entry["g_30sq10_l3"] = mean(g_sq_l3)
+            entry["g_30sq10_cc"] = mean(g_sq_cc)
+            entry["g_30sq10_largestclique"] = mean(g_sq_largestclique)
+            entry["g_30sq10_zlscc"] = mean(g_sq_zlscc)
+            entry["g_30sq10_zdiameter"] = mean(g_sq_zdiameter)
+            entry["g_30sq10_zaspl"] = mean(g_sq_zaspl)
+            entry["g_30sq10_zcc"] = mean(g_sq_zcc)
         else:
-            entry['g_30sq10_nodes'] = entry['g_sq_nodes']
-            entry['g_30sq10_edges'] = entry['g_sq_edges']
-            entry['g_30sq10_degree'] = entry['g_sq_degree']
-            entry['g_30sq10_density'] = entry['g_sq_density']
-            entry['g_30sq10_diameter'] = entry['g_sq_diameter']
-            entry['g_30sq10_aspl'] = entry['g_sq_aspl']
-            entry['g_30sq10_lscc'] = entry['g_sq_lscc']
-            entry['g_30sq10_pe'] = entry['g_sq_pe']
-            entry['g_30sq10_l1'] = entry['g_sq_l1']
-            entry['g_30sq10_l2'] = entry['g_sq_l2']
-            entry['g_30sq10_l3'] = entry['g_sq_l3']
-            entry['g_30sq10_cc'] = entry['g_sq_cc']
-            entry['g_30sq10_largestclique'] = entry['g_sq_largestclique']
-            entry['g_30sq10_zlscc'] = entry['g_sq_zlscc']
-            entry['g_30sq10_zdiameter'] = entry['g_sq_zdiameter']
-            entry['g_30sq10_zaspl'] = entry['g_sq_zaspl']
-            entry['g_30sq10_zcc'] = entry['g_sq_zcc']
+            entry["g_30sq10_nodes"] = entry["g_sq_nodes"]
+            entry["g_30sq10_edges"] = entry["g_sq_edges"]
+            entry["g_30sq10_degree"] = entry["g_sq_degree"]
+            entry["g_30sq10_density"] = entry["g_sq_density"]
+            entry["g_30sq10_diameter"] = entry["g_sq_diameter"]
+            entry["g_30sq10_aspl"] = entry["g_sq_aspl"]
+            entry["g_30sq10_lscc"] = entry["g_sq_lscc"]
+            entry["g_30sq10_pe"] = entry["g_sq_pe"]
+            entry["g_30sq10_l1"] = entry["g_sq_l1"]
+            entry["g_30sq10_l2"] = entry["g_sq_l2"]
+            entry["g_30sq10_l3"] = entry["g_sq_l3"]
+            entry["g_30sq10_cc"] = entry["g_sq_cc"]
+            entry["g_30sq10_largestclique"] = entry["g_sq_largestclique"]
+            entry["g_30sq10_zlscc"] = entry["g_sq_zlscc"]
+            entry["g_30sq10_zdiameter"] = entry["g_sq_zdiameter"]
+            entry["g_30sq10_zaspl"] = entry["g_sq_zaspl"]
+            entry["g_30sq10_zcc"] = entry["g_sq_zcc"]
 
+        df_strgfeatures = pd.concat(
+            [df_strgfeatures, pd.DataFrame([entry])], ignore_index=True
+        )
 
-        df_strgfeatures = pd.concat([df_strgfeatures, pd.DataFrame([entry])], ignore_index=True)
-
-df_strgfeatures.to_csv(root_data+'Remora-2023//Batch-1//features//6b1_sequential_graph_features.csv')
-
-        
+df_strgfeatures.to_csv(
+    root_data + "Remora-2023//Batch-1//features//6b1_sequential_graph_features.csv"
+)
 
 
 # In[ ]:
@@ -315,7 +334,7 @@ dynamic_entry
 # In[84]:
 
 
-entry['g_sq_zlscc']
+entry["g_sq_zlscc"]
 
 
 # In[50]:
@@ -324,11 +343,11 @@ entry['g_sq_zlscc']
 l2 = 0
 l3 = 0
 for node1 in nx.nodes(G_seq):
-    #print(node1)
+    # print(node1)
     for node2 in nx.neighbors(G_seq, node1):
-        #print('\t', node2)
+        # print('\t', node2)
         if node1 in nx.neighbors(G_seq, node2):
-            #print('diad')
+            # print('diad')
             l2 += 1
         for node3 in nx.neighbors(G_seq, node2):
             if node1 in nx.neighbors(G_seq, node3):
@@ -357,7 +376,3 @@ for each_item in nx.selfloop_edges(G_seq):
 
 
 # In[ ]:
-
-
-
-
---
+++ Feat_extraction/dict_utils.py
@@ -0,0 +1,125 @@
+import pandas as pd
+
+
+# SENTIMENT DICTIONARIES
+def get_sent_dics():
+    sent_dic = {}
+    valence_dic = {}
+    arousal_dic = {}
+    dominance_dic = {}
+    vad_df = pd.read_csv("sentiment_dictionaries//VAD dictionary.csv").drop(
+        "Unnamed: 0", axis=1
+    )
+    vad_df["Word"] = vad_df["Word"].astype(str)
+    for index, row in vad_df.iterrows():
+        valence_dic[row["Word"].lower()] = row["V.Mean.Sum"]
+        arousal_dic[row["Word"].lower()] = row["A.Mean.Sum"]
+        dominance_dic[row["Word"].lower()] = row["D.Mean.Sum"]
+
+    with open(
+        "sentiment_dictionaries//NRC-Emotion-Lexicon-Wordlevel-v0.92.txt", "r"
+    ) as sentiment_doc:
+        for each_line in sentiment_doc.readlines():
+            # print(each_line.strip().split('\t'))
+            each_sent = each_line.strip().split("\t")
+            sent_dic[(each_sent[0].lower(), each_sent[1])] = int(each_sent[2])
+    return sent_dic, valence_dic, arousal_dic, dominance_dic
+
+
+# Lexical Dictionaries
+def get_lex_dics():
+    aoa_df = pd.read_csv("lexical_dictionaries//1_AoA_Prevalence.csv")
+    aoa_dic = {}
+    aoa_df["Word"] = aoa_df["Word"].astype(str)
+    for index, row in aoa_df.iterrows():
+        for feature in ["NSyll", "AoA"]:
+            aoa_dic[(row["Word"].lower(), feature)] = row[feature]
+
+    semd_df = pd.read_csv("lexical_dictionaries//2_SemD.csv")
+    semd_df["item"] = semd_df["item"].astype(str)
+    semd_dic = {}
+    for index, row in semd_df.iterrows():
+        for feature in [
+            "mean_cos",
+            "SemD",
+        ]:  # ,'BNC_wordcount','BNC_contexts','BNC_freq','lg_BNC_freq'
+            semd_dic[(row["item"].lower(), feature)] = row[feature]
+
+    calg_conc_dic = {}
+
+    calg_conc = pd.read_excel("lexical_dictionaries//3_CalgaryConcrete.xlsx")
+    calg_conc["Word"] = calg_conc["Word"].astype(str)
+    for index, row in calg_conc.iterrows():
+        calg_conc_dic[row["Word"].lower()] = row["WordType"]
+
+    rate_conc_dic = {}
+
+    rate_conc = pd.read_excel("lexical_dictionaries//4_ConcRate.xlsx")
+    rate_conc["Word"] = rate_conc["Word"].astype(str)
+    for index, row in rate_conc.iterrows():
+        rate_conc_dic[row["Word"].lower()] = row["Conc.M"]
+
+    prevalence_dic = {}
+    prevalence_df = pd.read_excel("lexical_dictionaries//5_prevalence.xlsx")
+    prevalence_df["Word"] = prevalence_df["Word"].astype(str)
+    for index, row in prevalence_df.iterrows():
+        prevalence_dic[row["Word"].lower()] = row["Prevalence"]
+
+    iconicity_dic = {}
+    iconicity_df = pd.read_csv("lexical_dictionaries//6_iconicity.csv")
+    iconicity_df["word"] = iconicity_df["word"].astype(str)
+    for index, row in iconicity_df.iterrows():
+        iconicity_dic[row["word"].lower()] = row["rating"]
+
+    sensorimotor = [
+        "Auditory.mean",
+        "Gustatory.mean",
+        "Haptic.mean",
+        "Interoceptive.mean",
+        "Olfactory.mean",
+        "Visual.mean",
+        "Foot_leg.mean",
+        "Hand_arm.mean",
+        "Head.mean",
+        "Mouth.mean",
+        "Torso.mean",
+    ]
+    sensorimotor_dic = {}
+    sensorimotor_df = pd.read_csv("lexical_dictionaries//7_sensorimotor.csv")
+    for index, row in sensorimotor_df.iterrows():
+        for each_item in sensorimotor:
+            sensorimotor_dic[(row["Word"].lower(), each_item)] = row[each_item]
+
+    subtlex = ["SUBTLWF", "SUBTLCD"]
+    subtlex_dic = {}
+    subtlex_df = pd.read_excel("lexical_dictionaries//8_subtlex.xlsx")
+    subtlex_df["Word"] = subtlex_df["Word"].astype(str)
+    for index, row in subtlex_df.iterrows():
+        for each_item in subtlex:
+            subtlex_dic[(row["Word"].lower(), each_item)] = row[each_item]
+
+    taboo_dic = {}
+    taboo_df = pd.read_csv("lexical_dictionaries//9_taboo.csv")
+    for index, row in taboo_df.iterrows():
+        taboo_dic[row["Word"].lower()] = row["Taboo"]
+
+    glasgow = ["IMAG", "GEND", "SIZE"]
+    glasgow_dic = {}
+    glasgow_df = pd.read_csv("lexical_dictionaries//10_glasgow.csv")
+    glasgow_df["Word"] = subtlex_df["Word"].astype(str)
+    for index, row in glasgow_df.iterrows():
+        if index != 0:
+            for each_item in glasgow:
+                glasgow_dic[(row["Words"].lower(), each_item)] = float(row[each_item])
+    return (
+        aoa_dic,
+        semd_dic,
+        calg_conc_dic,
+        rate_conc_dic,
+        prevalence_dic,
+        iconicity_dic,
+        sensorimotor_dic,
+        subtlex_dic,
+        taboo_dic,
+        glasgow_dic,
+    )
